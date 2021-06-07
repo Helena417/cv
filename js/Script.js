6976feb0d9cbbd289a551bd8cdcdfc00a2994e75
Helena <susan2017966@gmail.com>
2021-06-08 06:25:44 +0800

    add padding slide

--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,29 @@
+// padding icon hide
+function Icon_hidden(obj) {
+  if (obj.style.display == 'block') {
+    obj.style.display = 'none';
+  } else {
+    obj.style.display = 'block';
+  }
+}
+function padding_slide(obj) {}
 
+window.onload = function () {
+  let dsn = document.getElementById('design');
+  let cod = document.getElementById('code');
+  let btnL = document.getElementById('left-btn');
+  let btnR = document.getElementById('right-btn');
+  let ds = document.getElementById('design-slide');
+  let cs = document.getElementById('code-slide');
+
+  dsn.onclick = function () {
+    Icon_hidden(btnR);
+    padding_slide(ds);
+    // return false;
+  };
+  cod.onclick = function () {
+    Icon_hidden(btnL);
+    padding_slide(cs);
+    // return false;
+  };
+};
